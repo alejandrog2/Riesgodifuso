abcb98d7a5db3b61cfc87e2f804e4cb7cc3ca25c
XuasyM <xsmeneses@gmail.com>
2015-11-06 14:12:44 -0500

    a
    ay vamos !

--- prog.js
+++ prog.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+var icfes;
+function validarIcfes(){
+	icfes = document.getElementById("icfes").value;             
+	if (icfes=="" ){
+		alert("Debe llenar todos los campos");
+	}if(isNaN(icfes)){
+		alert("El resultado debe ser una cantidad en números");
+                //document.getElementById("cont1").style.display = 'block';
+               //document.getElementById("cont2").style.display = 'none';---->debe abrir la otra ventana 
+           };if(!(icfes="")){
+           	  //document.getElementById("cont1").style.display = 'block';
+               //document.getElementById("cont2").style.display = 'none';---->debe abrir la otra ventana 
+           }
+       }
